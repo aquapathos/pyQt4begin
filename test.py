e8b5c346fa6e47403ef0f949dd578541b65cc862
aquapathos <firedwater@gmail.com>
2016-02-25 11:38:47 +0900

    no message

--- test.py
+++ test.py
@@ -8,10 +8,20 @@ This is a temporary script file.
 import sys
 import PyQt4.QtGui as QtGui
 
+
+def on_click():
+    pbutton.setText(" ^o^ ")
+    
 def main():
+    global pbutton
     app = QtGui.QApplication(sys.argv)
     mainW = QtGui.QMainWindow()
+
     pbutton = QtGui.QPushButton("Hollo")
+    def on_click():
+        pbutton.setText("World!")
+
+    pbutton.clicked.connect(on_click)
     mainW.setCentralWidget(pbutton)
     mainW.show()
     app.exec_()
